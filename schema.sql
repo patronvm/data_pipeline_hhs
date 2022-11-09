157310f0184504fedcc9ce4c07eee91e89005361
patronvm <patronum.768@gmail.com>
2022-11-09 13:29:18 -0500

    Added comment

--- schema.sql
+++ schema.sql
@@ -1,21 +1,17 @@
-/* We created tables based on properties, location, and 
-the number of beds in the hospitals
+/* We created tables based on the entities of the properties and location,
+rating, and the number of beds in the hospitals. We chose these entities
+based on what information being updated and grouping relevant information
+together.
 */
+
 
 /* The Hospital table contains inforamtion about general 
 properties of hospital. We set the unique ID for each hospital 
-as primary key. Attributes in the entity are related but
-independent to each other.
+as primary key. The table include location information. 
+Notice that though city, fips_code, state, and zip attributes 
+are dependent, it is convenient to have those attributes in a
+single table without a join.
 */
-
-/* The Location table contains inforamtion about the location
-of hospital. The table have hospital_id attribute references 
-the primary key of Hospital table, where we can join them in
-order to reduce redundancy. Notice that though city, fips_code,
-state, and zip attributes are dependent, it is convenient to
-have them in a single table without a join.
-*/
-
 CREATE TABLE Hospital (
 	hospital_pk VARCHAR PRIMARY KEY,
 	hospital_name VARCHAR, 
@@ -32,13 +28,20 @@ CREATE TABLE Hospital (
 	zip INTEGER	
 );
 
+/* The Rating table contains hospital ratings that update multiple
+times a year. These update will be recorded in this table.
+We assign continuous id as primary key to identify a specific version
+of rating. We can reference the Hospital table with the foreign key
+*/
 CREATE TABLE Rating (
+	id SERIAL PRIMARY KEY,
 	day DATE,
 	rating INTEGER CHECK (rating > 0),
 	hospital VARCHAR REFERENCES Hospital
 );
 
-
+/*The Hospital_beds table contains information about hospital bed
+status. They are updated with weekly information*/
 CREATE TABLE Hospital_beds (
 	all_adult_hospital_beds_7_day_avg FLOAT, 
 	all_pediatric_inpatient_beds_7_day_avg FLOAT,
