3116d247e9af5a86c249a18eb8c0c2ee37be23b3
Evana <wuyifan@YW.local>
2022-11-17 07:13:58 -0500

    sql command and connection

--- load_hhs.py
+++ load_hhs.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sys
-import psycopg2
+import psycopg
 
 
 def data_handle(df):
@@ -24,8 +24,72 @@ def data_handle(df):
     return df
 
 
-def run_sql():
-    pass
+def run_sql(df):
+    # Connect
+    conn = psycopg.connect(
+        host="sculptor.stat.cmu.edu",
+        dbname="wu3",
+        user="wu3",
+        password="Beshiez8o"
+    )
+    cur = conn.cursor()
+
+    with conn.transaction():
+        for index, row in df.iterrows():
+            try:
+                cur.execute("INSERT INTO Hospital (hospital_pk, hospital_name,\
+                            longitude, latitude, address, city, fips_code, zip)"
+                                "VALUES ('{0}', '{1}', {2}, {3}, '{4}', '{5}', '{6}', '{7}')"\
+                                    .format(row['hospital_pk'], row['hospital_name'],
+                                    row['longitude'], row['latitude'],
+                                    row['address'], row['city'],
+                                    row['fips_code'], row['zip']))
+            except Exception as e:
+                try:
+                    cur.execute("UPDATE Hospital"
+                                    "SET hospital_name = '{0}',\
+                                    longitude = {1},\
+                                    latitude = {2},\
+                                    address = '{3}',\
+                                    city = '{4}',\
+                                    fips_code = '{5}',\
+                                    zip = '{6}'"
+                                    "WHERE hospital_pk = '{7}'".format
+                                    (row['hospital_name'], row['longitude'],
+                                        row['latitude'], row['address'],
+                                        row['city'], row['fips_code'],
+                                        row['zip'], row['hospital_pk']))
+                except Exception as e:
+                    print("insert and update failed:", e)
+
+    with conn.transaction():
+        for index, row in df.iterrows():
+            try:
+                cur.execute("INSERT INTO Hospital_beds\
+                        (all_adult_hospital_beds_7_day_avg,\
+                        all_pediatric_inpatient_beds_7_day_avg,\
+                        all_adult_hospital_inpatient_bed_occupied_7_day_coverage,\
+                        all_pediatric_inpatient_bed_occupied_7_day_avg,\
+                        total_icu_beds_7_day_avg,\
+                        icu_beds_used_7_day_avg,\
+                        inpatient_beds_used_covid_7_day_avg,\
+                        staffed_adult_icu_patients_confirmed_covid_7_day_avg,\
+                        hospital)"
+                                "VALUES ({}, {}, {}, {}, {}, {}, {}, {}, '{}')"\
+                                .format(row['all_adult_hospital_beds_7_day_avg'],
+                                row['all_pediatric_inpatient_beds_7_day_avg'],
+                                row['all_adult_hospital_inpatient_bed_occupied_7_day_coverage'],
+                                row['all_pediatric_inpatient_bed_occupied_7_day_avg'],
+                                row['total_icu_beds_7_day_avg'],
+                                row['icu_beds_used_7_day_avg'],
+                                row['inpatient_beds_used_covid_7_day_avg'],
+                                row['staffed_icu_adult_patients_confirmed_covid_7_day_avg'],
+                                row['hospital_pk']))
+            except Exception as e:
+                print("insert failed:", e)
+
+    conn.commit()
+    conn.close()
 
 
 # Load data
@@ -33,4 +97,4 @@ path_name = sys.argv[1]
 df = pd.read_csv(path_name)
 df = data_handle(df)
 # SQL
-run_sql()
+run_sql(df[1:10])
